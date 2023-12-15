d00ec3933ec3b4984f1a3fc285c68e8a36ec4f1a
kangmin lee <growbook91@gmail.com>
2023-12-15 20:10:26 +0900

    Spring boot project for e-commerce web is initialized

---
+++ src/test/java/me/grobook91/ecommerceweb/ECommerceWebApplicationTests.java
@@ -0,0 +1,13 @@
+package me.grobook91.ecommerceweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommerceWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
